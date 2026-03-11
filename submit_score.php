7aa7d033f7adb6685708a41a4589b5a9ae3ab7d9
crabu321-sudo <crabu321@gmail.com>
2026-03-11 22:14:53 +0530

    Update submit_score.php

--- submit_score.php
+++ submit_score.php
@@ -1,10 +1,17 @@
 <?php
 header("Content-Type: application/json");
 
-$conn = new mysqli("localhost", "root", "", "game_scores");
+/* Render database connection */
+$host = "dpg-d6ms0975r7bs73cl4690-a";
+$user = "scores_o92q_user";
+$pass = "qb7GWiXnEjHRTSYNPtJJQ4KZ6PW6jJDz";
+$db   = "scores_o92q";
+$port = 5432;   // sometimes render uses 5432
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
-    die(json_encode(["status"=>"error"]));
+    die(json_encode(["status"=>"error","message"=>"DB Connection Failed"]));
 }
 
 $game  = $_POST['game'] ?? '';
@@ -18,8 +25,9 @@ if ($game && $name && is_numeric($score)) {
     $stmt->execute();
 
     echo json_encode(["status"=>"success"]);
+
 } else {
-    echo json_encode(["status"=>"error"]);
+    echo json_encode(["status"=>"error","message"=>"Invalid Data"]);
 }
 
 $conn->close();
